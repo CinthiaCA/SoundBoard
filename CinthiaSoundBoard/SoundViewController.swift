b5185a4675183b8894d6557988892f9c51845a27
Cinthia Calla Ayma <cinthia.calla@tecsup.edu.pe>
2022-10-24 16:10:25 -0500

    Final

---
+++ CinthiaSoundBoard/SoundViewController.swift
@@ -0,0 +1,29 @@
+//
+//  SoundViewController.swift
+//  CinthiaSoundBoard
+//
+//  Created by Mac 05 on 24/10/22.
+//
+
+import UIKit
+
+class SoundViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
